2cf47d0c5f3ad791003d0259cede5a07f20d2ef3
marshall.long@yale.edu <marshall.long@yale.edu@e068d6cc-edb8-4e28-8308-4603fcd97593>
2012-04-19 12:00:13 +0000

    Moved the prefix/suffix buffers into the global UIData structure Added DataView subclass to allow drawing in data window -- this still has some issues (it draws int the command window too) used Analyze tool add some release cases where memory leaks were indicated Mouse click and drag added to data window and xyz values added to status windows -- coordinates are wrong though

---
+++ trunk/oma2/oma2/DataView.h
@@ -0,0 +1,16 @@
+//
+//  DataView.h
+//  oma2
+//
+//  Created by Marshall Long on 4/19/12.
+//  Copyright (c) 2012 Yale University. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface DataView : NSImageView{
+    NSPoint startPoint,endPoint;
+
+}
+
+@end
