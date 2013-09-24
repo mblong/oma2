269271f275dc50dc5760519c4bc459f9e9ddefd1
marshall.long <marshall.long@yale.edu>
2013-09-24 22:01:17 +0000

    more small draw bits

--- oma2/oma2/dcrawx.mm
+++ oma2/oma2/dcrawx.mm
@@ -8333,7 +8333,7 @@ int dcrawGlue(char* name, int thecolor, Image* im){
 		puts("");
 		return -1;
 	}
-/*
+
 	argv[argc] = "";
 	for (arg=1; (((opm = argv[arg][0]) - 2) | 2) == '+'; ) {
 		opt = argv[arg++][1];
@@ -8393,7 +8393,7 @@ int dcrawGlue(char* name, int thecolor, Image* im){
 				return -1;
 		}
 	}
-*/	
+	
 	// -- oma
 
     if(document_mode) {
