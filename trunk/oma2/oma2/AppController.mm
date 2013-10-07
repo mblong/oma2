087fcd2860c211681a6f1f9861b37b33ea0995b9
marshall.long@yale.edu <marshall.long@yale.edu@e068d6cc-edb8-4e28-8308-4603fcd97593>
2013-10-07 13:36:01 +0000

    New classes added associated with DrawingWindow added — these are for line graphics Menu items added for plot rows and plot columns Can now copy a data window

--- trunk/oma2/oma2/AppController.mm
+++ trunk/oma2/oma2/AppController.mm
@@ -9,6 +9,7 @@
 #import "AppController.h"
 #import "PreferenceController.h"
 #import "DataWindowController.h"
+#import "DrawingWindowController.h"
 #import "StatusController.h"
 #import "ImageBitmap.h"
 
@@ -58,11 +59,105 @@ extern Image iBuffer;
     [preferenceController fillInUIData];
 }
 
+- (IBAction)plotRows:(id)sender{
+    NSWindow* activekey = [NSApp keyWindow];
+    NSWindow* activemain = [NSApp mainWindow];
+    int key = -1;
+    int main = -1;
+    int i=0;
+    for (DataWindowController* thewindow in windowArray){
+        if( [thewindow window ] == activekey) key=i;
+        i++;
+    }
+    i=0;
+    for (DataWindowController* thewindow in windowArray){
+        if( [thewindow window ] == activemain) main=i;
+        i++;
+    }
+    if ([[windowArray objectAtIndex: key] isKindOfClass:[DataWindowController class]]){
+        NSLog(@"%d %d ",key,main);
+    }
+
+ // figure out where to place image
+ // window_placement needs to have the right position and size
+    NSSize theWindowSize = [ [ activemain contentView ] frame ].size;
+ 
+    int windowHeight = 256;
+    int windowWidth = theWindowSize.width;
+/*
+ int windowWidth = iBitmap.getwidth();
+ float scaleWidth = (float)windowWidth/(float)screenRect.size.width;
+ // leave a little space at the bottom of the sreen
+ float scaleHeight = (float)windowHeight/(float)(screenRect.size.height-2*TITLEBAR_HEIGHT);
+ float scaleWindow = 1.0;
+ if (scaleHeight > 1.0 || scaleWidth > 1.0) {
+ if(scaleHeight > scaleWidth)
+ scaleWindow = scaleHeight;
+ else
+ scaleWindow = scaleWidth;
+ windowHeight /= scaleWindow;
+ windowWidth /= scaleWindow;
+ char txt[128];
+ sprintf(txt," Window scaled by %f\n",scaleWindow);
+ [self appendCText:txt];
+ 
+ }
+ 
+ // now, figure out where to place the window
+ if(window_placement.origin.x == WINDOW_OFFSET+screenRect.origin.x) {   // left column
+ window_placement.origin.y -= (windowHeight+TITLEBAR_HEIGHT);
+ }
+ 
+ window_placement=NSMakeRect(window_placement.origin.x,
+ window_placement.origin.y,
+ windowWidth, windowHeight+TITLEBAR_HEIGHT);
+ 
+ if (window_placement.origin.x+windowWidth>screenRect.size.width) {
+ window_placement.origin.x = screenRect.origin.x + WINDOW_OFFSET;
+ 
+ if(window_placement.origin.y - windowHeight - TITLEBAR_HEIGHT > 0){
+ window_placement.origin.y -= (windowHeight + TITLEBAR_HEIGHT);
+ } else{
+ wraps++;
+ window_placement.origin.y = screenRect.size.height
+ -windowHeight- wraps*TITLEBAR_HEIGHT; // wrap to top
+ }
+ 
+ }
+ 
+ // create a new window controller object
+ DataWindowController* dataWindowController = [[DataWindowController alloc] initWithWindowNibName:@"DataWindow"];
+ 
+ // add that to the array of windows
+ [windowArray addObject:dataWindowController];
+ 
+ // name the window appropriately
+ if(*windowname){
+ NSString *text  = [[NSString alloc] initWithCString:windowname encoding:NSASCIIStringEncoding];
+ [dataWindowController setWindowName:text] ;
+ } else{
+ [dataWindowController setWindowName:@"Data"] ;
+ }
+ 
+ // display the data
+ [dataWindowController placeImage:window_placement];
+ 
+ window_placement.origin.x += windowWidth;            // increment for next one
+ 
+ [dataWindowController showWindow:self];
+
+ */
+    //NSLog(@"%d %d ",key,main);
+    
+    
+}
+
+
 // this attempt to be notified when text changes in prefixes doesn't work
 // It gets called once only when the window opens, not when the text changes
-- (void)observeValueForKeyPath:(NSString *)keyPath 
-                      ofObject:(id)object 
-                        change:(NSDictionary *)change 
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
                        context:(void *)context {
     if ([keyPath isEqualToString:@"macroPrefix"]) {
         
@@ -71,12 +166,6 @@ extern Image iBuffer;
     }
 }
 
-
-
-
--(id) whoami{
-    return self;
-}
 
 -(void) appendText:(NSString *) string{
     last_return += [string length];
---
+++ trunk/oma2/oma2/DrawingView.mm
@@ -0,0 +1,22 @@
+//
+//  DrawingView.m
+//  oma2
+//
+//  Created by Marshall Long on 10/6/13.
+//  Copyright (c) 2013 Yale University. All rights reserved.
+//
+
+#import "DrawingView.h"
+#import "AppController.h"
+#import "ImageBitmap.h"
+#import "UI.h"
+
+
+extern ImageBitmap iBitmap;
+extern Image iBuffer;
+extern AppController* appController;
+extern oma2UIData UIData;
+
+@implementation DrawingView
+
+@end
