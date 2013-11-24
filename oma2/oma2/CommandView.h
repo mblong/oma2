3dced0886fc469aea4394b8e05ef6f4a024841c0
marshall.long <marshall.long@yale.edu>
2013-11-24 15:40:50 +0000

    Try running comdec in a different thread to allow UI updates and interruption in a macro comdec is changed (actually changed back) so it does not return to the caller when a macro is being performed (until the macro is done) added CommandThread object that contains the code that runs in the other thread This version doesn’t work hardly at all — the command thread accesses methods in the main thread, which isn’t allowed. All kinds of problems here.

--- oma2/oma2/CommandView.h
+++ oma2/oma2/CommandView.h
@@ -8,16 +8,21 @@
 
 #import <Cocoa/Cocoa.h>
 #import "oma2.h"
+#import "CommandThread.h"
 
-
+@class CommandThread;
 
 @interface CommandView : NSTextView{
     char oma2Command[CHPERLN];
 }
 
 @property NSUInteger lastReturn;
+@property CommandThread   *commandThread;
 
 -(void) appendText:(NSString *) string;
 -(void) appendCText:(char *) string;
 -(void) textDidChange:(NSNotification *) pNotify;
+
+- (int)scheduleCommand: (NSString*) theCommand;
+
 @end
