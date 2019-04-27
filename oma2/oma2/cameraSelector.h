efa83497938eb00cec7f3cd6f409a14d7bd7ac80
Marshall Long <marshall.long@yale.edu>
2019-04-27 15:49:22 -0400

    Changes to oma2cam. Currently assumes gphoto2 is installed, uses dynamic libraries for GIGE, doesn't require liblabjackusb.dylib to be in /usr/local/lib

--- oma2/oma2/cameraSelector.h
+++ oma2/oma2/cameraSelector.h
@@ -9,17 +9,31 @@
 #ifndef oma2_cameraSelector_h
 #define oma2_cameraSelector_h
 
+/* Note about external dynamic libraries:
+
+ mMst use the install_name_tool to modify dylibs to be relative to where they can be found in oma2cam.app
+ They are copied to the included Frameworks folder in the app Contents
+ This could be done in a script in a separate build phase, but once it's done it won't have to be done again as long as the dylibs that are part of the project are used.
+ 
+ example of some script commands:
+ # this ensures so the embedded dynamic libraries are found
+ export DYLIB=liblabjackusb.dylib
+ install_name_tool -id @executable_path/../Frameworks/$DYLIB $SRCROOT/oma2/$DYLIB
+ #install_name_tool -change $SRCROOT/oma2/$DYLIB @executable_path/../Frameworks/$DYLIB "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
+*/
+
 // select cameras here
 
-//#define GIGE_     // issues here with current Xcode
+#define GIGE_     //
 #define SBIG
 #define GPHOTO      // requires installing gphoto2 and its libraries: e.g., brew install gphoto2
+                    // if this is undefined, remove -lgphoto2 from Build Settings>Linking>Other Linker Flags
 
 // end of camera selection section
 
 // select other hardware here
 
-#define LJU3                // requires copying liblabjackusb.dydlb to /usr/local/lib or /opt/local/lib
+#define LJU3                // 
 #define SERIAL_PORT
 //#define VISA              // no longer supported
 
